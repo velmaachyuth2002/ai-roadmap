66a62788abe683c600bc5ebcd2adea4254e8edaa
Achyuth Velma <achyuthr37@gmail.com>
2026-07-17 15:41:58 -0600

    week2 day5: recent counter with deque queue

---
+++ week2/day5_queue.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+class RecentCounter:
+    def __init__(self):
+        self.q = deque()
+
+    def ping(self, t: int) -> int:
+        self.q.append(t)
+        while self.q and self.q[0]<t-3000:
+            self.q.popleft()
+        return len(self.q)
+rc = RecentCounter()
+
+print(rc.ping(1))      
+print(rc.ping(100))    
+print(rc.ping(3001))   
+print(rc.ping(3002))   
