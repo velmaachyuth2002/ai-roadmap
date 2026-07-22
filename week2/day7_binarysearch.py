9e95a4abb9cede3b2e0e7f2366a8f1cf5359ff96
Achyuth Velma <achyuthr37@gmail.com>
2026-07-21 22:39:13 -0600

    week2 day9: binary search

---
+++ week2/day7_binarysearch.py
@@ -0,0 +1,28 @@
+def binary_search(nums: list[int], target: int) -> int:
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+print(binary_search([1, 3, 5, 7, 9, 11], 7))   # 3
+print(binary_search([1, 3, 5, 7, 9, 11], 1))   # 0
+print(binary_search([1, 3, 5, 7, 9, 11], 11))  # 5
+print(binary_search([1, 3, 5, 7, 9, 11], 4))   # -1
+print(binary_search([], 5))                    # -1
+print(binary_search([5], 5))                   # 0
+
+
+
+big = list(range(1_000_000))
+
+index = binary_search(big, 999_999)
+print(index)
